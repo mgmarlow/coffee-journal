510d6ea4bdd72d0703e61265426727f51f51fa58
Graham Marlow <mgmarlow@gmail.com>
2020-01-18 15:24:59 -0800

    add cors

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql')
 
@@ -64,6 +65,7 @@ const root = {
 }
 
 const app = express()
+app.use(cors())
 app.use(
   '/graphql',
   graphqlHTTP({
