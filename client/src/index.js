fd0ad08add481e90e30bcfb4393e8d2fb13687d7
Graham Marlow <mgmarlow@gmail.com>
2020-01-19 16:55:16 -0800

    add bulma

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './index.scss'
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
